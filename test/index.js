121c793250a184a298c35405dc089c2e28f6b3f2
Igor Borysyuk <borysyuk@qarea.com>
2015-12-15 17:20:52 +0200

    tests setup

--- test/index.js
+++ test/index.js
@@ -1,13 +1,22 @@
 /**
  * Created by Ihor Borysyuk on 15.12.15.
  */
+require('babel-core/register');
 
 import { expect, should } from 'chai'
-import jsdom from 'mocha-jsdom'
 import _ from 'lodash'
+import App from '../src/scripts/containers/App'
+
+import React from 'react/addons'
+
+const TestUtils = React.addons.TestUtils;
+const shallowRenderer = TestUtils.createRenderer();
 
 describe('App', () => {
   it('test', () => {
+    shallowRenderer.render(React.createElement(App))
+    const component = shallowRenderer.getRenderOutput();
+    console.log(component);
     expect([]).to.deep.equal([])
   })
 
