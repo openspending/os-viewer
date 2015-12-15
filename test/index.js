f63d8ebc7c1c24687482b6823243ff567769e77c
Igor Borysyuk <borysyuk@qarea.com>
2015-12-15 17:20:33 +0200

    tests setup

---
+++ test/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Ihor Borysyuk on 15.12.15.
+ */
+
+import { expect, should } from 'chai'
+import jsdom from 'mocha-jsdom'
+import _ from 'lodash'
+
+describe('App', () => {
+  it('test', () => {
+    expect([]).to.deep.equal([])
+  })
+
+})
+
