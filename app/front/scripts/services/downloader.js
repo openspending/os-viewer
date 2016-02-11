621a8369686feb4cf8d6ead443bd9cf9abe275d0
Igor Borysyuk <borysyuk@qarea.com>
2016-02-11 11:23:39 +0200

    Accepting url params and changing it when something has been changed into UI

--- app/front/scripts/services/downloader.js
+++ app/front/scripts/services/downloader.js
@@ -8,6 +8,7 @@
 
   app.factory('downloader', ['$q', '_', '$http', function($q, _, $http) {
     var proxy = 'http://gobetween.oklabs.org/pipe/{url}';
+    var _cache = [];
     function getUrl(url, options) {
       return (options.proxy)? options.proxy.replace('{url}', encodeURIComponent(url)): url
     }
@@ -16,12 +17,16 @@
       get: function(url){
         var deferred = $q.defer();
 
-        fetch(getUrl( url , {proxy: proxy})).then(function(response){
-          return response.text();
-        }).then(function (text){
-          deferred.resolve(text);
-        });
-
+        if (_cache[url]){
+          deferred.resolve(_cache[url]);
+        } else {
+          fetch(getUrl( url , {proxy: proxy})).then(function(response){
+            return response.text();
+          }).then(function (text){
+            _cache[url] = text;
+            deferred.resolve(text);
+          });
+        }
         return deferred.promise;
       }
     }
