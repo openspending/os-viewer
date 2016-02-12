726b1cdcabc70435d5b564f48d26b7a36530edd6
Adam Kariv <adam.kariv@gmail.com>
2016-02-12 13:15:21 +0200

    Geoview Vis (merging Feature/geoview #69 from @kravets-levko)

--- app/front/scripts/application.js
+++ app/front/scripts/application.js
@@ -2,11 +2,15 @@
   var redux = require('redux/dist/redux.js');
   var ngRedux = require('ng-redux/dist/ng-redux.js');
 
+  var customComponents = require('components');
+
   angular.module('Application', [
     'ngBabbage',
     'angular.filter',
     'ui.bootstrap'
 //    'ngRedux'
   ]);
+
+  customComponents.babbageGeoView(angular.module('ngBabbage'));
 
 })(angular);
