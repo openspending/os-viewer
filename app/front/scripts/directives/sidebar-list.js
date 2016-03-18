36ee5e195e019e04b0d4df4dc91cabde36370b8d
Levko Kravets <kravets@qarea.com>
2016-03-18 20:23:24 +0200

    #874 Visualizations

--- app/front/scripts/directives/sidebar-list.js
+++ app/front/scripts/directives/sidebar-list.js
@@ -2,28 +2,35 @@
 
   var app = angular.module('Application');
 
-  app.directive('sidebarList', function() {
-    return {
-      templateUrl: 'templates/sidebar-list.html',
-      replace: true,
-      transclude: false,
-      restrict: 'E',
-      scope: {
-        items: '=',
-        selected: '=',
-        title: '@',
-        canClear: '@?'
-      },
-      link: function($scope) {
-        $scope.selectItem = function($event, item) {
-          $scope.$emit('sidebarList.changeItemSelection', item, true);
-          $event.stopPropagation();
-        };
-        $scope.unselectItem = function($event, item) {
-          $scope.$emit('sidebarList.changeItemSelection', item, false);
-          $event.stopPropagation();
-        };
-      }
-    };
-  });
+  app.directive('sidebarList', [
+    '_',
+    function(_) {
+      return {
+        templateUrl: 'templates/sidebar-list.html',
+        replace: false,
+        restrict: 'E',
+        scope: {
+          items: '=',
+          selected: '=',
+          title: '@',
+          canClear: '@?'
+        },
+        link: function($scope) {
+          $scope.isSelected = function(key) {
+            return !_.isUndefined(_.find($scope.selected, function(item) {
+              return item == key;
+            }));
+          };
+          $scope.selectItem = function($event, item) {
+            $scope.$emit('sidebarList.changeItemSelection', item, true);
+            $event.stopPropagation();
+          };
+          $scope.unselectItem = function($event, item) {
+            $scope.$emit('sidebarList.changeItemSelection', item, false);
+            $event.stopPropagation();
+          };
+        }
+      };
+    }
+  ]);
 })(angular);
---
+++ app/front/scripts/directives/visualizations.js
@@ -0,0 +1,56 @@
+;(function(angular) {
+
+  var app = angular.module('Application');
+
+  app.directive('visualizations', [
+    '_',
+    function(_) {
+      return {
+        templateUrl: 'templates/visualizations.html',
+        replace: false,
+        restrict: 'E',
+        scope: {
+          state: '=',
+          events: '='
+        },
+        link: function($scope, element) {
+          $scope.selectedVisualizations = [];
+
+          var modal = element.find('.x-visualization-add-modal').modal({
+            show: false
+          });
+
+          $scope.showAddVisualizationDialog = function() {
+            modal.modal('show');
+          };
+
+          $scope.addVisualization = function(visualization) {
+            var alreadyAdded = !!_.find(
+              $scope.selectedVisualizations,
+              function(item) {
+                return item == visualization;
+              }
+            );
+
+            if (!alreadyAdded) {
+              $scope.selectedVisualizations.push(visualization);
+            }
+          };
+
+          $scope.removeVisualization = function(visualization) {
+            $scope.selectedVisualizations = _.filter(
+              $scope.selectedVisualizations,
+              function(item) {
+                return item != visualization;
+              }
+            );
+          };
+
+          $scope.removeAllVisualizations = function() {
+            $scope.selectedVisualizations = [];
+          };
+        }
+      };
+    }
+  ]);
+})(angular);
