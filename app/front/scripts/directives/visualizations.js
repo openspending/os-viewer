29ebedd232f05805629e4b8fe10b5bbac22e0faf
Levko Kravets <kravets@qarea.com>
2016-03-21 12:37:34 +0200

    #874 Visualizations area fixes

--- app/front/scripts/directives/visualizations.js
+++ app/front/scripts/directives/visualizations.js
@@ -2,6 +2,39 @@
 
   var app = angular.module('Application');
 
+  var availableVisualizations = [
+    {
+      id: 'Treemap',
+      name: 'Tree Map',
+      type: ''
+    },
+    {
+      id: 'PieChart',
+      name: 'Pie Chart',
+      type: ''
+    },
+    {
+      id: 'BarChart',
+      name: 'Bar Chart',
+      type: ''
+    },
+    {
+      id: 'LineChart',
+      name: 'Line Chart',
+      type: ''
+    },
+    {
+      id: 'BubbleTree',
+      name: 'Bubble Tree',
+      type: ''
+    },
+    {
+      id: 'Map',
+      name: 'Map',
+      type: ''
+    }
+  ];
+
   app.directive('visualizations', [
     '_',
     function(_) {
@@ -14,7 +47,29 @@
           events: '='
         },
         link: function($scope, element) {
-          $scope.selectedVisualizations = ['Treemap'];
+          $scope.availableVisualizations = availableVisualizations;
+          $scope.selectedVisualizations = [];
+
+          $scope.getAvailableVisualizations = function() {
+            var canShowMap = false;
+            if ($scope.state && $scope.state.availablePackages) {
+              canShowMap = $scope.state.availablePackages.locationAvailable &&
+                $scope.state.availablePackages.locationSelected;
+            }
+
+            return _.filter(availableVisualizations, function(item) {
+              if (item.id == 'Map') {
+                return canShowMap;
+              }
+              return true;
+            });
+          };
+
+          $scope.getVisualizationById = function(visualization) {
+            return _.find(availableVisualizations, function(item) {
+              return item.id == visualization;
+            });
+          };
 
           var modal = element.find('.x-visualization-add-modal').modal({
             show: false
