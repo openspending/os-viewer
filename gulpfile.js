de572b03304a180463122da86135c2b71c661fa7
Levko Kravets <levko.ne@gmail.com>
2016-04-15 12:47:29 +0300

    Fixed project dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -112,7 +112,7 @@ gulp.task('embedded.styles', function() {
 gulp.task('vendor.scripts', function() {
   var files = [
     path.join(nodeModulesDir, '/js-polyfills/xhr.js'),
-    path.join(nodeModulesDir, '/bootstrap/dist/js/bootstrap.min.js'),
+    path.join(nodeModulesDir, '/os-bootstrap/dist/js/bootstrap.min.js'),
     path.join(nodeModulesDir, '/angular/angular.min.js'),
     path.join(nodeModulesDir, '/angular-animate/angular-animate.min.js'),
     path.join(nodeModulesDir, '/angular-filter/dist/angular-filter.min.js'),
@@ -126,7 +126,7 @@ gulp.task('vendor.scripts', function() {
 gulp.task('vendor.styles', function() {
   var files = [
     path.join(nodeModulesDir, '/font-awesome/css/font-awesome.min.css'),
-    path.join(nodeModulesDir, '/bootstrap/dist/css/bootstrap.min.css'),
+    path.join(nodeModulesDir, '/os-bootstrap/dist/css/bootstrap.min.css'),
     path.join(nodeModulesDir, '/angular/angular-csp.css'),
     path.join(nodeModulesDir, '/babbage.ui/dist/lib.css'),
     path.join(nodeModulesDir, '/bubbletree/dist/bubbletree.css'),
@@ -140,7 +140,7 @@ gulp.task('vendor.styles', function() {
 gulp.task('vendor.fonts', function() {
   var files = [
     path.join(nodeModulesDir, '/font-awesome/fonts/*'),
-    path.join(nodeModulesDir, '/bootstrap/dist/fonts/*')
+    path.join(nodeModulesDir, '/os-bootstrap/dist/fonts/*')
   ];
   return gulp.src(files)
     .pipe(gulp.dest(publicFontsDir));
@@ -148,9 +148,9 @@ gulp.task('vendor.fonts', function() {
 
 gulp.task('app.assets', function() {
   return gulp.src([
-      path.join(nodeModulesDir, '/bootstrap/dist/assets/os-branding/vector/light/os.svg'),
-      path.join(nodeModulesDir, '/bootstrap/dist/assets/os-branding/vector/light/viewer.svg'),
-      path.join(nodeModulesDir, '/bootstrap/dist/assets/os-branding/vector/light/osviewer.svg'),
+      path.join(nodeModulesDir, '/os-bootstrap/dist/assets/os-branding/vector/light/os.svg'),
+      path.join(nodeModulesDir, '/os-bootstrap/dist/assets/os-branding/vector/light/viewer.svg'),
+      path.join(nodeModulesDir, '/os-bootstrap/dist/assets/os-branding/vector/light/osviewer.svg'),
   ])
     .pipe(gulp.dest(publicAssetsDir));
 });
@@ -165,7 +165,7 @@ gulp.task('app.fonts', function() {
 
 gulp.task('app.favicon', function() {
   var files = [
-    path.join(nodeModulesDir, '/bootstrap/dist/assets/os-branding/viewer-favicon.ico')
+    path.join(nodeModulesDir, '/os-bootstrap/dist/assets/os-branding/viewer-favicon.ico')
   ];
   return gulp.src(files)
     .pipe(rename('favicon.ico'))
