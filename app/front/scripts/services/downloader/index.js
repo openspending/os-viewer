bb4abac8e6a3fa3e777b99956ff306278011640a
Levko Kravets <levko.ne@gmail.com>
2016-08-10 23:29:03 +0300

    openspending/openspending#1061 Improve query cache

--- app/front/scripts/services/downloader/index.js
+++ app/front/scripts/services/downloader/index.js
@@ -2,13 +2,36 @@
 
 require('isomorphic-fetch');
 var Promise = require('bluebird');
+var url = require('url');
+var normalizeUrl = require('normalize-url');
 
 var cache = {};
 
+// This function should return properly formatted and encoded url.
+// `originalUrl` may have or have not url-encoded query or its parts,
+// path, may have default port (i.e. :80 for http://). This function
+// will convert all such variants to a single form.
+function getNormalizedUrl(originalUrl) {
+  if (originalUrl.indexOf('://') == -1) {
+    return originalUrl;
+  }
+  originalUrl = normalizeUrl(originalUrl, {
+    stripWWW: false
+  });
+  var parsed = url.parse(originalUrl);
+  parsed.pathname = decodeURIComponent(parsed.pathname);
+  return url.format(parsed);
+}
+
 module.exports = {
   get: function(url) {
+    // Make cache more efficient - use normalized url
+    url = getNormalizedUrl(url);
     if (!cache[url]) {
       cache[url] = fetch(url).then(function(response) {
+        if (response.status != 200) {
+          throw 'Failed loading data from ' + response.url;
+        }
         return response.text();
       });
     }
@@ -17,6 +40,13 @@ module.exports = {
     });
   },
   getJson: function(url) {
-    return this.get(url).then(JSON.parse);
+    return this.get(url)
+      .then(JSON.parse)
+      .catch(function(error) {
+        console.trace(error);
+      })
+  },
+  flush: function() {
+    cache = {};
   }
 };
