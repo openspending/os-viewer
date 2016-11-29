288999052e09978761c5f2e430f18c0ec78b2720
Levko Kravets <levko.ne@gmail.com>
2016-11-29 13:56:30 +0200

    Improve localization-related code

--- download_translations.py
+++ download_translations.py
@@ -1,25 +1,49 @@
 import csv
 import json
+import os
 import requests
 import codecs
 
 URL = 'https://docs.google.com/spreadsheets/d/1AkXyiIR0DYXRSk4_3h5NtUUR_D_1MHvnZTw6xNqdI2U/pub?gid=0&single=true&output=csv'
 
-reader = csv.reader(codecs.iterdecode(
-    requests.get(URL, stream=True).iter_lines(), 'utf-8'
-))
-header = next(reader)
-languages = ['en'] + header[2:]
-ret = {'xx': {}}
-for row in reader:
-    for language, translation in zip(languages, row[1:]):
-        ret.setdefault(language,{})[row[1]] = translation
-        ret['xx'][row[1]] = row[1].upper()
 
-translations = json.load(open('app/config/translations.json', 'r'))
-translations.update(ret)
+def load_local(path):
+    result = {}
+    for filename in os.listdir(path):
+        language = filename[:-5]  # strip .json
+        result[language] = json.load(open(path + '/' + filename, 'r'))
 
-json.dump(
-    translations, open('app/config/translations.json', 'w'),
-    sort_keys=True, indent=2, separators=(',', ': ')
-)
+    return result
+
+
+def load_spreadsheet(url):
+    reader = csv.reader(codecs.iterdecode(
+        requests.get(url, stream=True).iter_lines(), 'utf-8'
+    ))
+
+    languages = next(reader)[2:]
+    result = dict(zip(languages, [{} for n in range(len(languages))]))
+
+    for row in reader:
+        key = row[1]
+        row = list(zip(languages, row[2:]))
+        for language, translation in row:
+            if translation != '':
+                result[language][key] = translation
+
+    return result
+
+
+def main():
+    result = {}
+    result.update(load_local('translations'))
+    result.update(load_spreadsheet(URL))
+
+    json.dump(
+        result,
+        open('app/config/translations.json', 'w'),
+        sort_keys=True, indent=2, separators=(',', ': ')
+    )
+
+
+main()
