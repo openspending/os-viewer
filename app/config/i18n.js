655763ef640d60ea604f770cbcfe62341386cdf3
Adam Kariv <adam.kariv@gmail.com>
2017-02-05 11:04:04 +0200

    More translations

--- app/config/i18n.js
+++ app/config/i18n.js
@@ -5,7 +5,13 @@ var Polyglot = require('node-polyglot');
 
 module.exports.init = function(lang) {
   var translations = require('./translations.json');
-  var strings = _.extend({}, translations.en, translations[lang]);
+  var locale_parts = lang ? lang.split('_') : [];
+  var strings;
+  if (locale_parts.length > 1) {
+    strings = _.extend({}, translations.en, translations[locale_parts[0]], translations[lang]);
+  } else {
+    strings = _.extend({}, translations.en, translations[lang]);
+  }
   var polyglot = new Polyglot();
   polyglot.extend(strings);
   return polyglot.t.bind(polyglot);
