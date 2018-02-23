e33ba29744687df56ed044e0a130f2d44766f963
Brook Elgie <specialbrew@gmail.com>
2018-02-23 12:53:03 +0000

    Use .env file and lookenv

---
+++ lookenv.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // Required settings
+  // OS_EXTERNAL_ADDRESS: {required: true},
+
+  // Optional settings
+  OS_SNIPPETS_GA: {required: false},
+  OS_SNIPPETS_RAVEN: {required: false},
+  SENTRY_DSN: {required: false}
+
+  // Each service will use OS_BASE_URL unless overridden by these:
+  // OS_CONDUCTOR_URL: {required: false},
+  // OS_SEARCH_URL: {required: false},
+  // OS_VIEWER_URL: {required: false},
+  // OS_EXPLORER_URL: {required: false},
+  // OS_PACKAGER_URL: {required: false}
+};
