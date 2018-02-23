7f9599dba54e09517d806b3e9af9f55380c1a37a
Brook Elgie <specialbrew@gmail.com>
2018-02-23 15:54:09 +0000

    Update env var keys for platform consistency

--- app/config/index.js
+++ app/config/index.js
@@ -3,20 +3,18 @@
 var path = require('path');
 var nconf = require('nconf');
 
-var DEFAULT_HOST = 'https://openspending.org';
+var DEFAULT_HOST = process.env.OS_BASE_URL;
 var DEFAULT_BASE_PATH = '';
 
 nconf.file({
   file: path.join(__dirname, '/../../settings.json')
 });
 
-var apiHost = process.env.OS_VIEWER_API_HOST || DEFAULT_HOST;
-var authHost = process.env.OS_VIEWER_AUTH_HOST || DEFAULT_HOST;
-var searchHost = process.env.OS_VIEWER_SEARCH_HOST || DEFAULT_HOST;
+var apiHost = process.env.OS_API_URL || DEFAULT_HOST;
+var authHost = process.env.OS_CONDUCTOR_URL || DEFAULT_HOST;
+var searchHost = process.env.OS_SEARCH_URL || DEFAULT_HOST + '/search/package';
 var dataMineHost = process.env.OS_VIEWER_DATAMINE_HOST || DEFAULT_HOST;
-
-var cosmopolitanHost = process.env.OS_VIEWER_API_COSMO_HOST ||
-  '//cosmopolitan.openspending.org/v1/';
+var cosmopolitanHost = process.env.OS_VIEWER_API_COSMO_HOST;
 
 // this is the object that you want to override in your own local config
 nconf.defaults({
@@ -30,7 +28,7 @@ nconf.defaults({
     cosmoUrl: cosmopolitanHost
   },
   search: {
-    url: searchHost + '/search/package'
+    url: searchHost
   },
   dataMine: {
     url: dataMineHost
--- lookenv.config.js
+++ lookenv.config.js
@@ -1,16 +1,22 @@
 module.exports = {
   // Required settings
-  // OS_EXTERNAL_ADDRESS: {required: true},
+  OS_BASE_URL: {required: true},
 
   // Optional settings
+  OS_VIEWER_BASE_PATH: {
+    default: 'viewer/'
+  },
   OS_SNIPPETS_GA: {required: false},
   OS_SNIPPETS_RAVEN: {required: false},
-  SENTRY_DSN: {required: false}
+  SENTRY_DSN: {required: false},
 
   // Each service will use OS_BASE_URL unless overridden by these:
-  // OS_CONDUCTOR_URL: {required: false},
-  // OS_SEARCH_URL: {required: false},
-  // OS_VIEWER_URL: {required: false},
-  // OS_EXPLORER_URL: {required: false},
-  // OS_PACKAGER_URL: {required: false}
+  OS_VIEWER_API_COSMO_HOST: {
+    default: '//cosmopolitan.openspending.org/v1/'
+  },
+  OS_API_URL: {required: false},
+  OS_EXPLORER_URL: {required: false},
+  OS_CONDUCTOR_URL: {required: false},
+  OS_SEARCH_URL: {required: false},
+  OS_VIEWER_DATAMINE_HOST: {required: false}
 };
